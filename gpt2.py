f364e98c51da8401b25a86308d6fcd734dd75093
AtulSehwag <atul.sehwag@data-smith.ai>
2026-09-02 17:43:07 +0530

    reduced parameters, optimised weight intialisation, moving towards training

--- gpt2.py
+++ gpt2.py
@@ -1,8 +1,10 @@
 import math
 import torch
+import tiktoken
 import torch.nn as nn
 from dataclasses import dataclass
 from torch.nn import functional as F
+from transformers import GPT2LMHeadModel
 
 
 class CausalSelfAttention(nn.Module):
@@ -13,6 +15,7 @@ class CausalSelfAttention(nn.Module):
         # K, Q and V proj for all heads, in a batch
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
         # regularization
         self.n_head = config.n_head
@@ -50,6 +53,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate="tanh")
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -100,7 +104,22 @@ class GPT(nn.Module):
         )
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-    def forward(self, idx):
+        self.transformer.wte.weight = self.lm_head.weight
+
+        self.apply(self._init_weights)
+    
+    def _init_weights(self,module):
+        if isinstance(module,nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer ) ** -0.5
+            torch.nn.init.normal_(module.weight, mean = 0.0, std = 0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets = None):
         # idx shape = (B,T)
         B,T = idx.size()
         assert T <= self.config.block_size, f"Cannot forward sequence of lenght {T}, block size is only {self.config.block_size}"
@@ -115,13 +134,16 @@ class GPT(nn.Module):
         
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x) #(B,T, vocab_size)
-        return logits
+
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1,logits.size(-1)), targets.view(-1))
+        return logits, loss
     
     @classmethod
     def from_pretrained(cls, model_type):
         """Loads pretrained GPT-2 model weights from huggingface"""
         assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
-        from transformers import GPT2LMHeadModel
 
         print("loading weights from pretrained gpt: %s" % model_type)
 
@@ -184,35 +206,81 @@ num_return_sequences = 5
 max_length = 30
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Using device: {device}")
 
-model = GPT.from_pretrained("gpt2")
-model.eval()
-model.to(device)
+# Loading the Hugging Face gpt2 weights into our model and inferencing
+# model = GPT.from_pretrained("gpt2")
+# model.eval()
+# model.to(device)
 
 # prefix tokens
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode("hello, I'm a language model")
-tokens = torch.tensor(tokens, dtype=torch.long) #(8,)
-tokens = tokens.unsqueeze(0).repeat(num_return_sequences,1) #(5,8)
-x = tokens.to(device)
+# tokens = enc.encode("hello, I'm a language model")
+# tokens = torch.tensor(tokens, dtype=torch.long) #(8,)
+# tokens = tokens.unsqueeze(0).repeat(num_return_sequences,1) #(5,8)
+# x = tokens.to(device)
 
-torch.manual_seed(42)
-if device.type == 'cuda':
-    torch.cuda.manual_seed(42)
+# torch.manual_seed(42)
+# if device.type == 'cuda':
+#     torch.cuda.manual_seed(42)
 
-while x.size(1) < max_length:
-    with torch.no_grad():
-        logits = model(x)
-        logits = logits[:, -1, :]
-        probs = F.softmax(logits,dim=-1)
-        topk_probs, topk_indices = torch.topk(probs,50,-1)
-        ix = torch.multinomial(topk_probs, 1)
-        xcol = torch.gather(topk_indices, -1,ix)
-        x = torch.cat((x, xcol), dim=1)
+# while x.size(1) < max_length:
+#     with torch.no_grad():
+#         logits = model(x)
+#         logits = logits[:, -1, :]
+#         probs = F.softmax(logits,dim=-1)
+#         topk_probs, topk_indices = torch.topk(probs,50,-1)
+#         ix = torch.multinomial(topk_probs, 1)
+#         xcol = torch.gather(topk_indices, -1,ix)
+#         x = torch.cat((x, xcol), dim=1)
         
-for i in range(num_return_sequences):
-    tokens = x[i, :max_length].tolist()
-    decoded = enc.decode(tokens)
-    print(">",decoded)
+# for i in range(num_return_sequences):
+#     tokens = x[i, :max_length].tolist()
+#     decoded = enc.decode(tokens)
+#     print(">",decoded)
+
+# Training the model on custom data
+
+class DataloaderLite:
+    def __init__(self,B,T):
+        self.B = B
+        self.T = T
+    
+        with open("input.txt", "r") as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2') 
+        tokens = enc.encode(text)   
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens)//(B*T)} batches")
+
+        self.current_position = 0
+    
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position:self.current_position + B*T + 1]
+        x = buf[:-1].view(B,T)
+        y = buf[1:].view(B,T)
+
+        self.current_position += B*T
+        if self.current_position + (B*T + 1) > len(self.tokens):
+            self.current_position = 0
+        
+        return x,y
+
+train_loader = DataloaderLite(B=4, T=32)
+model = GPT(GPTConfig())
+model.to(device)
+print(f"Using device: {device}")
+
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-4)
+for i in range(50):
+    x,y = train_loader.next_batch()
+    x,y = x.to(device) , y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x,y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
